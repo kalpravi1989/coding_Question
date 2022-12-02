47a045ab3a239a4983ecd50d3180248f6a8a32ac
kalpravi1989 <kalpravi1989@gmail.com>
2022-12-02 15:13:27 -0600

    print statement program

---
+++ src/test/java/javaQues_03/PrintStatements.java
@@ -0,0 +1,18 @@
+package javaQues_03;
+
+import java.util.Scanner;
+
+public class PrintStatements {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello Programing");
+		sc.nextLine();
+		System.out.println("i don't like you");
+		sc.nextLine();
+		System.out.println("But I have to \"Learn\",I am ready");
+
+	}
+
+}
