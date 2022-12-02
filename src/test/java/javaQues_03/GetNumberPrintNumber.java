b10f5a77d1bb2d04c057a4000a95ad3bb1a7f4c6
kalpravi1989 <kalpravi1989@gmail.com>
2022-12-02 15:16:39 -0600

    Getnumber and Print number

---
+++ src/test/java/javaQues_03/GetNumberPrintNumber.java
@@ -0,0 +1,15 @@
+package javaQues_03;
+
+import java.util.Scanner;
+
+public class GetNumberPrintNumber {
+
+	public static void main(String[] args) {
+		System.out.println("enter number");
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Given number is " + n);
+
+	}
+
+}
